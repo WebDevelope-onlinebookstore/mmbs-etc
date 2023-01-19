1a7022c37c3d266f85a63e7a5a68ce00933090ba
tmddk9445 <tmddk9445@naver.com>
2023-01-19 21:32:42 +0900

    2023 01 19 THU LEE Database Column rename

--- mmbs_database.sql
+++ mmbs_database.sql
@@ -6,18 +6,18 @@ USE `mmbs_database`;
 -- Table `mmbs_database`.`orderpayment`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mmbs_database`.`orderpayment` (
-  `order_number` INT NOT NULL,
+  `order_number` INT NOT NULL AUTO_INCREMENT,
   `order_user_id` VARCHAR(45) NOT NULL,
   `order_product_isbn` INT NOT NULL,
-  `order_memberName` VARCHAR(45) NOT NULL,
-  `order_memberPhone` VARCHAR(45) NOT NULL,
-  `order_memberEmail` VARCHAR(45) NOT NULL,
+  `order_member_name` VARCHAR(45) NOT NULL,
+  `order_member_phone` VARCHAR(45) NOT NULL,
+  `order_member_email` VARCHAR(45) NOT NULL,
   `order_product_title` VARCHAR(45) NOT NULL,
   `order_product_price` INT NOT NULL,
   `order_product_amount` INT NOT NULL,
   `order_total_price` INT NOT NULL,
   `order_date` DATE NOT NULL,
-  `order_recipient` VARCHAR(45) NOT NULL,
+  `order_recieptient` VARCHAR(45) NOT NULL,
   `order_reciept_phone` VARCHAR(45) NOT NULL,
   `order_ship_address` VARCHAR(200) NOT NULL,
   `order_ship_price` INT NOT NULL,
@@ -32,22 +32,22 @@ COLLATE = utf8mb4_general_ci;
 -- -----------------------------------------------------
 -- Table `mmbs_database`.`product`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mmbs_database`.`product` (
-  `product_isbn` INT NOT NULL,
-  `product_title` text NOT NULL,
-  `product_writer` VARCHAR(45) NULL NOT NULL,
-  `product_publisher` VARCHAR(45) NOT NULL,
-  `product_age` VARCHAR(45) NOT NULL,
-  `product_genre` VARCHAR(45) NOT NULL,
-  `product_publication_date` DATE NOT NULL,
-  `product_price` INT NOT NULL,
-  `product_sales_price` INT NOT NULL,
-  `product_stock` INT NOT NULL,
-  `procudt_like` INT NOT NULL,
-  `product_stocking_date` DATE default NULL,
-  `product_introduce` text NOT NULL,
-  `product_image_url` text NOT NULL,
-  PRIMARY KEY (`product_isbn`))
+CREATE TABLE IF NOT EXISTS `mmbs_database`.`book` (
+  `book_isbn` INT NOT NULL,
+  `book_title` text NOT NULL,
+  `book_writer` VARCHAR(45) NULL NOT NULL,
+  `book_publisher` VARCHAR(45) NOT NULL,
+  `book_age` VARCHAR(45) NOT NULL,
+  `book_genre` VARCHAR(45) NOT NULL,
+  `book_publication_date` DATE NOT NULL,
+  `book_price` INT NOT NULL,
+  `book_sales_price` INT NOT NULL,
+  `book_stock` INT NOT NULL,
+  `book_like` INT NOT NULL,
+  `book_stocking_date` DATE default NULL,
+  `book_introduce` text NOT NULL,
+  `book_image_url` text NOT NULL,
+  PRIMARY KEY (`book_isbn`))
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8mb4
 COLLATE = utf8mb4_general_ci;
@@ -63,13 +63,13 @@ CREATE TABLE IF NOT EXISTS `mmbs_database`.`user` (
   `user_name` VARCHAR(45) NOT NULL,
   `user_phone` VARCHAR(45) NOT NULL,
   `user_email` VARCHAR(45) NOT NULL,
-  `user_addressApi` VARCHAR(200) NOT NULL,
-  `user_addressDtl` VARCHAR(200) NULL DEFAULT NULL,
+  `user_address_api` VARCHAR(200) NOT NULL,
+  `user_address_dtl` VARCHAR(200) NULL DEFAULT NULL,
   `user_grade` VARCHAR(45) NOT NULL,
   `user_total_amount` INT NOT NULL,
   `user_recommend` INT NOT NULL,
   `user_kid_birth` VARCHAR(45) NULL DEFAULT NULL,
-  `user_signup_date` DATE NOT NULL,
+  `user_sign_up_date` DATE NOT NULL,
   `user_withdraw` DATE NULL DEFAULT NULL,
   `user_coupon` VARCHAR(45) NULL DEFAULT NULL,
   PRIMARY KEY (`user_id`))
@@ -81,7 +81,7 @@ COLLATE = utf8mb4_general_ci;
 -- Table `mmbs_database`.`ask`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mmbs_database`.`ask` (
-	`ask_id` int NOT NULL,
+	`ask_id` int NOT NULL AUTO_INCREMENT,
 	`ask_writer` VARCHAR(45) NOT NULL,
 	`ask_title` VARCHAR(45) NOT NULL,
 	`ask_sort` VARCHAR(45) NOT NULL,
@@ -98,7 +98,7 @@ COLLATE = utf8mb4_general_ci;
 -- Table `mmbs_database`.`review`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mmbs_database`.`review` (
-	`review_id` int NOT NULL,
+	`review_id` int NOT NULL AUTO_INCREMENT,
 	`review_writer` VARCHAR(45) NOT NULL,
 	`order_number` INT NOT NULL,
 	`review_title` VARCHAR(45) NOT NULL,
@@ -115,7 +115,7 @@ COLLATE = utf8mb4_general_ci;
 -- Table `mmbs_database`.`cart`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mmbs_database`.`cart` (
-  `cart_id` int NOT NULL,
+  `cart_id` int NOT NULL AUTO_INCREMENT,
   `cart_user_id` VARCHAR(45) NOT NULL,
 	`cart_product_isbn` INT,
 	`cart_product_name` VARCHAR(45) NOT NULL,
@@ -156,7 +156,7 @@ COLLATE = utf8mb4_general_ci;
 -- -----------------------------------------------------
 
 CREATE TABLE `playtoy` (
-`playtoy_id` int NOT null,
+`playtoy_id` int NOT null AUTO_INCREMENT,
 `playtoy_title` text not null,
 `playtoy_age` varchar(45) not null,
 `playtoy_genre` varchar(45) not null,
@@ -176,7 +176,7 @@ COLLATE = utf8mb4_general_ci;
 -- -----------------------------------------------------
 
 CREATE TABLE `gift` (
-`gift_id` int NOT null,
+`gift_id` int NOT null AUTO_INCREMENT,
 `gift_sort` varchar(45),
 primary key (`gift_id`))
 
